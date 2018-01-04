084d173e241f2a0d20cef0a2613481d32ba9ef71
jbecerril <jbecerril@stengineeringsolutions.com>
2018-01-04 16:50:53 -0600

    Adding code to the first file

---
+++ code1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#---------------------------
+__author__ = "Jose Becerril"
+__copyright__ = "Copyright 2018, ST Project"
+__credits__ = ["Jose Becerril"]
+__license__ = "GPL 2"
+__version__ = "1.0.1"
+__maintainer__ = "JLBS"
+__email__ = "ingjoseluisbecerril@gmail.com"
+__status__ = "Debbug"
+__execution__ = "python code1.py"
+#---------------------------
+#Library
+import random
+
+#---------------------------
+#Code
+def mifirstHello():
+	print("HolaMundo")
+#---------------------------
+#Execution
+if __name__ == "__main__":
+	mifirstHello()
+#---------------------------
